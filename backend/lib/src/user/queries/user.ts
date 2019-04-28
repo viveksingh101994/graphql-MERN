f2359bcc30e34818161718074c96fe603cf9364c
vivek <vivek.singh@scheduleonce.com>
2019-04-29 00:16:49 +0530

    refactored code

--- backend/lib/src/user/queries/user.ts
+++ backend/lib/src/user/queries/user.ts
@@ -4,18 +4,16 @@ import { userModel, userType } from "../model";
 // Query
 export const userQuery = new GraphQLObjectType({
   name: "Query",
-  fields: () => {
-    return {
-      users: {
-        type: new GraphQLList(userType),
-        resolve: async () => {
-          const user = await userModel.find();
-          if (!user) {
-            throw new Error("error while fetching data");
-          }
-          return user;
+  fields: {
+    users: {
+      type: new GraphQLList(userType),
+      resolve: async () => {
+        const user = await userModel.find();
+        if (!user) {
+          throw new Error("error while fetching data");
         }
+        return user;
       }
-    };
+    }
   }
 });
